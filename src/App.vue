997c7582be78f8dc9bf7296a370ad0c49453bbc5
周琳 <837769723@qq.com>
2017-11-08 08:35:29 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style lang="stylus">
+  html, body, #app, input, textarea
+  font-family "PingFang SC", "Helvetica Neue", "Hiragino Sans GB", "Segoe UI", "Microsoft YaHei", "微软雅黑", sans-serif
+                                                                                                          -webkit-font-smoothing antialiased
+                                                                                                          -moz-osx-font-smoothing grayscale
+                                                                                                          height 100%
+
+  h2 {
+    display: block;
+    font-size: 1.5em;
+    -webkit-margin-before: 0.83em;
+    -webkit-margin-after: 0.83em;
+    -webkit-margin-start: 0px;
+    -webkit-margin-end: 0px;
+    font-weight: bold;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,48 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import iView from 'iview'
+import moment from 'moment'
+import vueMoment from 'vue-moment'
+import 'iview/dist/styles/iview.css'
+import '../theme/index.less'
+
+window.moment = moment
+Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
+Vue.use(iView)
+Vue.use(vueMoment)
+
+axios.interceptors.response.use(
+  response => {
+    if (response.data.code === 601) {
+      router.replace({
+        path: '/admin/login'
+      })
+    }
+    return response
+  },
+  error => {
+    return Promise.reject(error.response.data)
+  })
+
+router.beforeEach((to, from, next) => {
+  iView.LoadingBar.start()
+  next()
+})
+
+router.afterEach((to, from, next) => {
+  iView.LoadingBar.finish()
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
